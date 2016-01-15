b9c0de86245b16637091f43eebf5b3fc314c33f2
Magnus Thor <magnus@xsockets.net>
2016-01-15 13:34:02 +0100

    Service exanmple ,moved stuff into separate files
    Now contains a .service example.  Stuff moved into separate files.

--- example/js/app.js
+++ example/js/app.js
@@ -1,4 +1,4 @@
-﻿angular.module("myApp", ["xsockets","ngRoute"])
+﻿angular.module("myApp", ["xsockets", "ngRoute"])
     .config([
         "xsocketsControllerProvider", "$routeProvider","$provide", function (xsocketsControllerProvider, $routeProvider,$provide) {
             xsocketsControllerProvider.open("wss://rtcplaygrouund.azurewebsites.net:443");
@@ -14,65 +14,8 @@
             // provide the generic controller as bar "serviceId" 
             $provide.factory("bar", ["xsocketsController", function (xsocketsController) {
                 var bar = xsocketsController("generic");
-                console.log("bar provided", bar);
+            
                 return bar;
             }]);
         }
     ]);
-
-
-
-angular.module("myApp").controller("donkyController", ["$scope", "foo", function ($scope, rtc) {
-        $scope.message = {
-            text: ""
-        };
-        $scope.messages = [];
-        rtc.onopen = function (ci) {
-            console.log("donkyController rtc open",ci)
-        };
-
-        rtc.on("chatmessage", function (data) {
-            $scope.messages.unshift(data);
-        });
-        $scope.sendMessage = function (event) {
-            if (event === 13) {
-                rtc.invoke("chatmessage", {
-                    text: $scope.message.text, dt: new Date()
-                });
-                $scope.message.text = "";
-            }
-        };
-    }]).factory("foo", ["xsocketsController", function (xsocketsController) {
-        return xsocketsController("generic");
-    }]);
-   
-
-
- // 'bar' is provided using the "config" of myApp
-angular.module("myApp").controller("monkyController", ["$scope", "bar", function ($scope, rtc) {
-
-    $scope.message = {
-        text: ""
-    };
-    $scope.messages = [];
-    rtc.onopen = function (ci) {
-        console.log("monkyController rtc open", ci)
-    };
-    rtc.on("chatmessage", function (data) {
-        $scope.messages.unshift(data);
-    });
-    $scope.sendMessage = function (event) {
-        if (event === 13) {
-            rtc.invoke("chatmessage", {
-                text: $scope.message.text, dt: new Date()
-            });
-            $scope.message.text = "";
-        }
-    };
-}]);
-    
-
-
-
-
-
---
+++ example/js/services/myService.js
@@ -0,0 +1,17 @@
+﻿// 'bar' is provided using the "config" of myApp
+angular.module("myApp").service('myService', ["bar", function (bar) {
+
+    var self = this;
+
+    this.messages = [];
+
+    bar.on("chatmessage", function (data) {
+        self.messages.unshift(data);
+    });
+    this.sendMessage = function (message) {
+        bar.invoke("chatmessage", {
+            text: message, dt: new Date()
+        });
+    };
+
+}]);
