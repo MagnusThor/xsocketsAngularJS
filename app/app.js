94f48f8b4be6c74decc3d6d89140964b89171ea7
Magnus Thor <magnus@xsockets.net>
2016-01-15 13:50:22 +0100

    abs path fix

--- app/app.js
+++ app/app.js
@@ -2,14 +2,14 @@
     .config([
         "xsocketsControllerProvider", "$routeProvider", "$provide", function (xsocketsControllerProvider, $routeProvider, $provide) {
             xsocketsControllerProvider.open("wss://rtcplaygrouund.azurewebsites.net:443");
-            $routeProvider.when('/monky', {
-                templateUrl: '/app/view/monky.html',
-                controller: 'monkyController'
-            }).when('/donky', {
-                templateUrl: '/app/view/donky.html',
-                controller: 'donkyController'
+            $routeProvider.when("/monky", {
+                templateUrl: "app/view/monky.html",
+                controller: "monkyController"
+            }).when("/donky", {
+                templateUrl: "app/view/donky.html",
+                controller: "donkyController"
             }).otherwise({
-                redirectTo: '/monky'
+                redirectTo: "/monky"
             });
             $provide.factory("bar", ["xsocketsController", function (xsocketsController) {
                 var bar = xsocketsController("generic");
